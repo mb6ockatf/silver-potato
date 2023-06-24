4b2fe3fa3f960c256ecedb504b835978daf85a5d
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-06-25 00:26:02 +0300

    feat(squares table): added
    also, write comments to two powers table
    small fixes to bf preparator

--- bf_preparator.sh
+++ bf_preparator.sh
@@ -4,20 +4,20 @@ if [ $# -ne 2 ]; then
   echo "usage: $0 <input_file> <output_file>"
   exit 1
 fi
-if [ ! -f $1 ]; then
+if [ ! -f "$1" ]; then
   echo "error: input file does not exist"
   exit 1
 fi
 tmpfile=$(mktemp)
 while read -r line; do
-  for (( i=0; i<${#line}; i++ )); do
+  for ((i=0; i<${#line}; i++)); do
     char="${line:$i:1}"
-    if [[ "[=-+]><.," =~ "$char" ]]; then
+    if [[ "[=-+]><.," =~ $char ]]; then
       printf "%s" "$char" >> "$tmpfile"
     fi
   done
   printf "\n" >> "$tmpfile"
 done < "$1"
-cat "$tmpfile" | tr -d '\n\t\r' > "$2"
+cmd "$tmpfile" | tr -d '\n\t\r' > "$2"
 rm "$tmpfile"
 echo "done"
